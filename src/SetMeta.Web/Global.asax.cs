e1edd7169436ff9ca6df8de6461d33a3c0af0464
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-25 13:33:45 +0200

    initial configuration

---
+++ src/SetMeta.Web/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+using SetMeta.Web.Configurations;
+
+namespace SetMeta.Web
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            MvcHandler.DisableMvcResponseHeader = true;
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            PrepareIocContainer();
+        }
+
+        private void PrepareIocContainer()
+        {
+            var container = new WindsorContainer().Install(FromAssembly.This());
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container.Kernel));
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("X-Powered-By");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            HttpContext.Current.Response.Headers.Remove("X-AspNetMvc-Version");
+
+            HttpContext.Current.Response.Headers.Set("Server", string.Format("Web Server ({0}) ", Environment.MachineName));
+        }
+    }
+}
