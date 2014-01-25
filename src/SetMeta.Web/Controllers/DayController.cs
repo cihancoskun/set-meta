385d2acc824b8d98b112650137b7b097667eba22
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-25 13:44:12 +0200

    base meta entity added

---
+++ src/SetMeta.Web/Controllers/DayController.cs
@@ -0,0 +1,18 @@
+﻿namespace SetMeta.Web.Controllers
+{
+    public class DayController
+    {
+        //views
+        //todo:new
+        //todo:list
+        //todo:detail
+
+ 
+        //todo: json GET
+        /*
+         [HttpGet, AllowAnonymous]
+        public async Task<JsonResult> Index(string code)
+         */
+
+    }
+}
---
+++ src/SetMeta.Web/_Domain/Entities/BaseMeta.cs
@@ -0,0 +1,14 @@
+﻿namespace SetMeta.Web._Domain.Entities
+{
+    public class BaseMeta
+    {
+        public string Code { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Day : BaseMeta
+    {
+
+    }
+}
