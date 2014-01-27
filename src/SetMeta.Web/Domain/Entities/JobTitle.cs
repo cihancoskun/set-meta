0c8120603a1006433615a29574fa2b59b039581c
canerc <canercvs@gmail.com>
2014-01-27 12:07:41 +0200

    jobtitles meta data

---
+++ src/SetMeta.Web/Domain/Entities/JobTitle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetMeta.Web.Domain.Entities
+{
+    public class JobTitle : BaseMeta 
+    {
+
+    }
+}
--- src/SetMeta.Web/Domain/Repositories/SetMetaDbMigrationConfiguration.cs
+++ src/SetMeta.Web/Domain/Repositories/SetMetaDbMigrationConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.Entity.Migrations;
 using System.Linq;
+using System.Web.Mvc;
 using SetMeta.Web.Helpers;
 using SetMeta.Web.Domain.Entities;
 
@@ -50,6 +51,45 @@ namespace SetMeta.Web.Domain.Repositories
 
             #endregion
 
+            #region Job Titles
+
+            AddJobTitles(context, "General Manager", "general_manager");
+            AddJobTitles(context, "Software Developer", "software_developer");
+            AddJobTitles(context, "Logo Support", "logo_support");
+            AddJobTitles(context, "Department Chief", "department_chief");
+            AddJobTitles(context, "Secretary", "secretary");
+            AddJobTitles(context, "Administrative Manager", "administrative_manager");
+            AddJobTitles(context, "Service Manager", "service_manager");
+            AddJobTitles(context, "IT Manager", "it_manager");
+            AddJobTitles(context, "Technical Service", "technical_service");
+            AddJobTitles(context, "It Specialist", "it_specialist");
+            AddJobTitles(context, "Customer Representative", "customer_representative");
+            AddJobTitles(context, "Accounting Personnel", "accounting_personnel");
+            AddJobTitles(context, "Customer Services", "customer_services");
+            AddJobTitles(context, "Project Manager", "project_manager");
+            AddJobTitles(context, "Sales Representative", "sales_representative");
+            AddJobTitles(context, "Chief Executive Officer", "chief_executive_officer");
+            AddJobTitles(context, "Call Center Operator", "call_center_operator");
+            AddJobTitles(context, "Business Development Project Manager", "business_development_project_manager");
+            AddJobTitles(context, "Vice General Manager", "vice_general_manager");
+            AddJobTitles(context, "Purchasing Manager", "purchasing_manager");
+            AddJobTitles(context, "Manager", "manager");
+            AddJobTitles(context, "Assistant Manager", "assistant_manager");
+            AddJobTitles(context, "Quality Management Specialist", "quality_management_specialist");
+            AddJobTitles(context, "System Manager", "system_manager");
+            AddJobTitles(context, "Accounting Manager", "accounting_manager");
+            AddJobTitles(context, "Quality Assurance Manager", "quality_assurance_manager");
+            AddJobTitles(context, "IT Personnel", "it_personnel");
+            AddJobTitles(context, "Quality System manager", "quality_system_manager");
+            AddJobTitles(context, "General Secretary", "general_secretary");
+            AddJobTitles(context, "Vice President Corporate Affairs", "vice_president_corporate_affairs");
+            AddJobTitles(context, "President Corporate Affairs", "president_corporate_affairs");
+            AddJobTitles(context, "Rector", "rector");
+            AddJobTitles(context, "Coordinator", "coordinator");
+            AddJobTitles(context, "Operation Manager", "operation_manager");
+            AddJobTitles(context, "Administrator", "administrator");
+            #endregion
+
         }
 
         private void AddMonths(SetMetaDbContext context, string name, string key, string value)
@@ -61,6 +101,16 @@ namespace SetMeta.Web.Domain.Repositories
                 Value = value
             };
             context.Months.Add(month);
+        }
+
+        private void AddJobTitles(SetMetaDbContext context, string name, string key)
+        {
+            var jobTitle = new JobTitle
+            {
+                Code = key,
+                Name = name,
+            };
+            context.JobTites.Add(jobTitle);
         }
 
         private static void AddAdmin(SetMetaDbContext context, string name, string email)
