2f875529b6261b5daa2fa3bd35d15eec2adc4f6e
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-30 14:51:39 +0200

    new metadata form

---
+++ sources/Controllers/DataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SetMeta.Web.Helpers;
+using SetMeta.Web.ViewModels;
+
+namespace SetMeta.Web.Controllers
+{
+    public class DataController : BaseController
+    {
+        [HttpGet]
+        public ActionResult New()
+        {
+            return Redirect("/data/list");
+        }
+    }
+}
