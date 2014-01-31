2e66d91fef198354c1a6c517f09f4153c32206b0
Mehmet Sabancıoğlu <mehmet.sabancioglu@hotmail.com>
2014-01-31 15:11:04 +0200

    Added Create New Token Selenium Tests and New app

--- tests/UserInterface/AppUITests.cs
+++ tests/UserInterface/AppUITests.cs
@@ -12,28 +12,36 @@ namespace SetMeta.Tests.UserInterface
         [Test]
         public void should_add_new_app()
         {
-            _browser.Navigate().GoToUrl(string.Format("{0}/user/logout", BASE_URL));
-            _browser.Navigate().GoToUrl(string.Format("{0}/user/login", BASE_URL));
-            
-            _browser.FindElementById("email").SendKeys("mehmet.sabancioglu@gmail.com");
-            _browser.FindElementById("password").SendKeys("password");
-            _browser.FindElementById("frm").Submit();
-            
-            _browser.Navigate().GoToUrl(string.Format("{0}/app/new", BASE_URL));
-            
-            _browser.FindElementById("name").SendKeys(Guid.NewGuid().ToNoDashString());
-            _browser.FindElementById("url").SendKeys(Guid.NewGuid().ToNoDashString());
-            _browser.FindElementById("description").SendKeys(Guid.NewGuid().ToNoDashString());
-            _browser.FindElementById("frm").Submit();
-           
+            CreateApp();
+
             CloseBrowser();
         }
 
         [Test]
         public void should_create_new_token()
         {
+            CreateApp();
+
+            _browser.FindElementById("btnNewToken").Click();
 
             CloseBrowser();
+        }
+
+        private void CreateApp()
+        {
+            _browser.Navigate().GoToUrl(string.Format("{0}/user/logout", BASE_URL));
+            _browser.Navigate().GoToUrl(string.Format("{0}/user/login", BASE_URL));
+
+            _browser.FindElementById("email").SendKeys("mehmet.sabancioglu@gmail.com");
+            _browser.FindElementById("password").SendKeys("password");
+            _browser.FindElementById("frm").Submit();
+
+            _browser.Navigate().GoToUrl(string.Format("{0}/app/new", BASE_URL));
+
+            _browser.FindElementById("name").SendKeys(Guid.NewGuid().ToNoDashString());
+            _browser.FindElementById("url").SendKeys(Guid.NewGuid().ToNoDashString());
+            _browser.FindElementById("description").SendKeys(Guid.NewGuid().ToNoDashString());
+            _browser.FindElementById("frm").Submit();
         }
     }
 }
