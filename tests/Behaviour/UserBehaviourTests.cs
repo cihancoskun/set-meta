1a00e460e414aa7826900b4040061d56d1936a9b
Cihan Coşkun <cihancoskun@gmail.com>
2014-01-31 13:38:10 +0200

    added new tests for UserBehaviour

--- tests/Behaviour/UserBehaviourTests.cs
+++ tests/Behaviour/UserBehaviourTests.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using Moq;
 using NUnit.Framework;
+using SetMeta.Tests._Builders;
+using SetMeta.Tests._TestHelpers;
 using SetMeta.Web.Models;
 using SetMeta.Web.Services;
 
@@ -9,19 +13,107 @@ namespace SetMeta.Tests.Behaviour
     [TestFixture]
     public class UserBehaviourTests
     {
-        public void any_user_in_developer_role_can_create_app()
-        {
-            var validModel = new AppModel {Name = "name", Url = "url"};
-            var appService = new Mock<IAppService>();
+        const string ActionNameNew = "New";
+        const string ActionNameNewToken = "NewToken";
+        const string ActionNameDeleteToken = "DeleteToken";
 
+        [Test]
+        public async void any_user_in_developer_role_can_create_app()
+        {
+            //arrange
+            var validModel = new AppModel
+            {
+                Name = "name",
+                Url = "url"
+            };
+
+            var appService = new Mock<IAppService>();
             appService.Setup(x => x.CreateApp(validModel)).Returns(Task.FromResult(true));
 
-        }
-        public void any_user_in_developer_role_can_deactivate_app() { }
-        public void any_user_in_developer_role_can_activate_app() { }
+            //act
+            var sut = new AppControllerBuilder().WithAppService(appService.Object)
+                                                .BuildWithMockControllerContext();
 
-        public void any_user_in_developer_role_can_create_token() { }
-        public void any_user_in_developer_role_can_delete_token() { }
+            var result = await sut.New(validModel);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsAssignableFrom<RedirectResult>(result);
+
+            appService.Verify(x => x.CreateApp(validModel), Times.Once);
+
+            sut.AssertPostAttribute(ActionNameNew, new[] { typeof(AppModel) });
+        }
+
+        [Test]
+        public void any_user_in_developer_role_can_deactivate_app()
+        {
+            //arrange 
+
+            //act 
+
+            //assert 
+        }
+
+        [Test]
+        public void any_user_in_developer_role_can_activate_app()
+        {
+            //arrange 
+
+            //act 
+
+            //assert 
+        }
+
+        [Test]
+        public async void any_user_in_developer_role_can_create_token()
+        {
+            //arrange   
+            var appService = new Mock<IAppService>();
+
+            appService.Setup(x => x.CreateToken(It.IsAny<TokenModel>())).Returns(Task.FromResult(true));
+
+            //act
+            var sut = new AppControllerBuilder().WithAppService(appService.Object)
+                                                .BuildWithMockControllerContext();
+
+            var result = await sut.NewToken("1");
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Data);
+            Assert.IsAssignableFrom<JsonResult>(result);
+            Assert.IsAssignableFrom<ResponseModel>(result.Data);
+
+            appService.Verify(x => x.CreateToken(It.IsAny<TokenModel>()), Times.Once);
+
+            sut.AssertPostAttribute(ActionNameNewToken, new[] { typeof(string) });
+        }
+
+        [Test]
+        public async void any_user_in_developer_role_can_delete_token()
+        {
+            //arrange   
+            var appService = new Mock<IAppService>();
+
+            appService.Setup(x => x.DeleteToken(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(true));
+
+            //act
+            var sut = new AppControllerBuilder().WithAppService(appService.Object)
+                                                .BuildWithMockControllerContext();
+
+            var result = await sut.DeleteToken("1");
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Data);
+            Assert.IsAssignableFrom<JsonResult>(result);
+            Assert.IsAssignableFrom<ResponseModel>(result.Data);
+
+            appService.Verify(x => x.DeleteToken(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+
+            sut.AssertPostAttribute(ActionNameDeleteToken, new[] { typeof(string) });
+        }
 
         public void any_user_in_admin_role_can_create_app() { }
         public void any_user_in_admin_role_can_deactivate_app() { }
---
+++ tests/_Builders/AppControllerBuilder.cs
@@ -0,0 +1,55 @@
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using SetMeta.Web.Controllers;
+using SetMeta.Web.Services;
+
+namespace SetMeta.Tests._Builders
+{
+    public class AppControllerBuilder
+    { 
+        private IAppService _appService;
+
+        public AppControllerBuilder()
+        { 
+            _appService = null;
+        }
+          
+        internal AppControllerBuilder WithAppService(IAppService appService)
+        {
+            _appService = appService;
+            return this;
+        }
+
+        internal AppController BuildWithMockControllerContext()
+        {
+            var sut = Build();
+
+            var controllerContext = new Mock<ControllerContext>();
+            var httpContext = new Mock<HttpContextBase>();
+            var httpRequest = new Mock<HttpRequestBase>();
+            var httpResponse = new Mock<HttpResponseBase>();
+            var user = new Mock<IPrincipal>();
+            var currentUser = new Mock<IIdentity>();
+
+            controllerContext.Setup(x => x.HttpContext).Returns(httpContext.Object);
+            httpContext.Setup(x => x.Request).Returns(httpRequest.Object);
+            httpContext.Setup(x => x.Response).Returns(httpResponse.Object);
+            httpContext.Setup(x => x.User).Returns(user.Object);
+            user.Setup(x => x.Identity).Returns(currentUser.Object);
+            currentUser.Setup(x => x.IsAuthenticated).Returns(true);
+            currentUser.Setup(x => x.Name).Returns(string.Format("{0}|{1}|{2}|{3}", 1, "name", "test@test.com", 1));
+
+            httpResponse.Setup(x => x.SetCookie(It.IsAny<HttpCookie>()));
+
+            sut.ControllerContext = controllerContext.Object;
+            return sut;
+        }
+
+        internal AppController Build()
+        {
+            return new AppController(_appService);
+        }
+    }
+}
