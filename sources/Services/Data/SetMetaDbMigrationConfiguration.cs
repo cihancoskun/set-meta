8228cab80d9eb26c0beea681f06107a81b38ca92
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-31 15:13:11 +0200

    some meta data added

--- sources/Services/Data/SetMetaDbMigrationConfiguration.cs
+++ sources/Services/Data/SetMetaDbMigrationConfiguration.cs
@@ -32,6 +32,16 @@ namespace SetMeta.Web.Services.Data
 
             #endregion
 
+            #region Quarters
+
+            var quarterPublicId = AddMetaDataType(context, "Quarters");
+            AddMetaData(context, "Quarters", quarterPublicId, "First Quarter", "1");
+            AddMetaData(context, "Quarters", quarterPublicId, "Second Quarter", "2");
+            AddMetaData(context, "Quarters", quarterPublicId, "Third Quarter", "3");
+            AddMetaData(context, "Quarters", quarterPublicId, "Fourth Quarter", "4");
+
+            #endregion
+
             #region Months
 
             var monthPublicId = AddMetaDataType(context, "Months");
@@ -46,11 +56,211 @@ namespace SetMeta.Web.Services.Data
             AddMetaData(context, "Months", monthPublicId, "September", "9");
             AddMetaData(context, "Months", monthPublicId, "October", "10");
             AddMetaData(context, "Months", monthPublicId, "November", "11");
-            AddMetaData(context, "Months", monthPublicId, "December", "12"); 
+            AddMetaData(context, "Months", monthPublicId, "December", "12");
 
             #endregion
 
+            #region Days
+
+            var dayPublicId = AddMetaDataType(context, "Days");
+            AddMetaData(context, "Days", dayPublicId, "Monday", "1");
+            AddMetaData(context, "Days", dayPublicId, "Tuesday", "2");
+            AddMetaData(context, "Days", dayPublicId, "Wednesday", "3");
+            AddMetaData(context, "Days", dayPublicId, "Thursday", "4");
+            AddMetaData(context, "Days", dayPublicId, "Friday", "5");
+            AddMetaData(context, "Days", dayPublicId, "Saturday", "6");
+            AddMetaData(context, "Days", dayPublicId, "Sunday", "7");
+
+            #endregion
+
+            #region Timezones
+
+            var tiemZonesPublicId = AddMetaDataType(context, "Time Zones");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−12:00", "utc_minus_twelve");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−11:00", "utc_minus_eleven");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−10:00", "utc_minus_ten");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−09:30", "utc_minus_half_past_nine");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−09:00", "utc_minus_nine");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−08:00", "utc_minus_eight");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−07:00", "utc_minus_seven");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−06:00", "utc_minus_six");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−05:00", "utc_minus_five");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−04:30", "utc_minus_half_past_four");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−04:00", "utc_minus_four");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−03:30", "utc_minus_half_past_three");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−03:00", "utc_minus_three");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−02:00", "utc_minus_two");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC−01:00", "utc_minus_one");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC±00:00", "utc_zero");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+01:00", "utc_plus_one");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+02:00", "utc_plus_two");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+03:00", "utc_plus_three");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+03:30", "utc_plus_half_past_three");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+04:00", "utc_plus_four");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+04:30", "utc_plus_half_past_four");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+05:00", "utc_plus_five");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+05:30", "utc_plus_half_past_five");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+05:45", "utc_plus_a_quarter_to_five");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+06:00", "utc_plus_six");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+06:30", "utc_plus_half_past_six");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+07:00", "utc_plus_seven");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+08:00", "utc_plus_eight");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+08:45", "utc_plus_a_quarter_to_nine");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+09:00", "utc_plus_nine");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+09:30", "utc_plus_half_past_nine");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+10:00", "utc_plus_ten");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+10:30", "utc_plus_half_past_ten");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+11:00", "utc_plus_eleven");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+11:30", "utc_plus_half_past_eleven");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+12:00", "utc_plus_twelve");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+12:45", "utc_plus_a_quarter_to_thirteen");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+13:00", "utc_plus_thirteen");
+            AddMetaData(context, "Time Zones", tiemZonesPublicId, "UTC+14:00", "utc_plus_fourteen");
+
+            #endregion
+
+            #region Horoscopes
+
+            var horoscopesPublicId = AddMetaDataType(context, "Horoscopes");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Aquarius", "1");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Aries", "2");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Cancer", "3");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Capricorn", "4");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Gemini", "5");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Leo", "6");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Libra", "7");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Pisces", "8");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Sagittarius", "9");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Scorpio", "10");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Taurus", "11");
+            AddMetaData(context, "Horoscopes", horoscopesPublicId, "Virgo", "12");
+
+            #endregion
+
+            #region Vehicle Types
+
+            var vehicleTypesPublicId = AddMetaDataType(context, "Vehicle Types");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "Automobile", "1");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "SUVs & Pick-Up Trucks", "2");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "Motorcyles", "3");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "Minivans, Vans & Panelvans", "4");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "ATV", "5");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "UTV", "6");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "Electric Vehicles", "7");
+            AddMetaData(context, "Vehicle Types", vehicleTypesPublicId, "RVs & Campers", "8");
+
+            #endregion
+
+            #region Car Brands
+
+            var carBrandsPublicId = AddMetaDataType(context, "Car Brands");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Acura", "www.acura.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Alfa Romeo", "www.alfaromeo.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Aston Martin", "www.astonmartin.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Audi", "www.audi.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Bentley", "www.bentleymotors.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "BMW", "www.bmw.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Bugatti", "www.bugatti.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Buick", "www.buick.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Cadillac", "www.cadillac.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Chery", "www.chery.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Chevrolet", "www.chevrolet.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Chrysler", "www.chrysler.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Citroën", "www.citroen.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Dacia", "www.dacia.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Daewoo", "www.daewoous.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Daihatsu", "www.daihatsu.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Dodge", "www.dodge.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Eagle", "www.eagleautonz.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Ferrari", "www.ferrari.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Fiat", "www.fiat.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Ford", "www.ford.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "GAZ", "eng.gazgroup.ru");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Geely", "www.geely.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Gumpert", "www.gumpert.de");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Hennessey", "www.hennesseyperformance.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Honda", "honda.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Hyundai", "www.hyundai.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Ikco", "www.ikco.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Infiniti", "www.infiniti.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Isuzu", "www.isuzu.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Jaguar", "www.jaguar.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Kia", "www.kia.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lada", "www.lada.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lamborghini", "www.lamborghini.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lancia", "www.lancia.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lexus", "www.lexus.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lincoln", "www.lincoln.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Lotus", "www.lotuscars.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Maserati", "www.maserati.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Mazda", "www.mazda.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "McLaren", "www.mclaren.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Mercedes - Benz", "www.mercedes-benz.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Mercury", "www.mercuryvehicles.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "MG", "mg.co.uk");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Mini", "www.mini.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Mitsubishi", "www.mitsubishi-motors.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Nissan", "www.nissan-global.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Oldsmobile", "www.oldsmobile.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Opel", "www.opel.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Peugeot", "www.peugeot.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Pontiac", "www.pontiac.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Porsche", "www.porsche.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Proton", "www.proton.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Renault", "www.renault.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Rolls-Royce", "www.rolls-roycemotorcars.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Saab", "www.saab.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Seat", "www.seat.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Skoda", "www.skoda-auto.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Smart", "www.smart.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Subaru", "www.subaru-global.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Suzuki", "www.suzuki.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Şamil", "www.samil.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Tata", "www.tatamotors.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Toyota", "www.toyota.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Volkswagen", "www.vw.com");
+            AddMetaData(context, "Car Brands", carBrandsPublicId, "Volvo", "www.volvo.com");
+
+            #endregion
+
+            #region Payment Type
+
+            var paymentTypePublicId = AddMetaDataType(context, "Payment Type");
+            AddMetaData(context, "Payment Type", paymentTypePublicId, "Cash", "1");
+            AddMetaData(context, "Payment Type", paymentTypePublicId, "Credit Card", "2");
+            AddMetaData(context, "Payment Type", paymentTypePublicId, "Paypal", "3");
+
+            #endregion
+
+            #region Credit Card
+
+            var creditCardPublicId = AddMetaDataType(context, "Credit Card");
+            AddMetaData(context, "Credit Card", creditCardPublicId, "American Express", "american_express");
+            AddMetaData(context, "Credit Card", creditCardPublicId, "Diners Club", "diners_club");
+            AddMetaData(context, "Credit Card", creditCardPublicId, "MasterCard", "master_card");
+            AddMetaData(context, "Credit Card", creditCardPublicId, "Visa", "visa");
+
+            #endregion
+
+            #region Continents
+
+            var continentsPublicId = AddMetaDataType(context, "Continents");
+            AddMetaData(context, "Continents", continentsPublicId, "Asia", "1");
+            AddMetaData(context, "Continents", continentsPublicId, "Africa", "2");
+            AddMetaData(context, "Continents", continentsPublicId, "Antarctica", "3");
+            AddMetaData(context, "Continents", continentsPublicId, "North America", "4");
+            AddMetaData(context, "Continents", continentsPublicId, "Europe", "5");
+            AddMetaData(context, "Continents", continentsPublicId, "Oceania", "6");
+            AddMetaData(context, "Continents", continentsPublicId, "South America", "7");
+
+            #endregion
+
+
+
             #region Countries
+
+            var countryPublicId = AddMetaDataType(context, "Countries");
+
 
             #endregion
 
