9725446e562bc28e718fc8004727bc6297d988af
Cihan Coşkun <cihancoskun@gmail.com>
2014-01-31 18:47:07 +0200

    added FeedbackUITest. but not working (=

--- tests/UserInterface/FeedbackUITests.cs
+++ tests/UserInterface/FeedbackUITests.cs
@@ -3,8 +3,20 @@
 namespace SetMeta.Tests.UserInterface
 {
     [TestFixture]
-    public class FeedbackUITests
+    public class FeedbackUITests : BaseUITest
     {
-         
+        [Test]
+        private void open_feed_back_pop_up()
+        {
+            LogOut();
+            _browser.Navigate().GoToUrl(string.Format("{0}/user/login", BASE_URL));
+
+            _browser.FindElementById("email").SendKeys("mehmet.sabancioglu@gmail.com");
+            _browser.FindElementById("password").SendKeys("password");
+            _browser.FindElementById("frm").Submit();
+
+            _browser.FindElementById("btn_open_feed_back").Click();
+        }
+
     }
 }
